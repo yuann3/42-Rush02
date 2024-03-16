cba37da53364f4262fba9753517ea9d7db9fde88
Justin Lee <triplez@justinlee.sg>
2024-03-16 17:21:34 +0800

    add check*.*

--- include/check/check_dict_file.h
+++ include/check/check_dict_file.h
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   check_dict_file.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/16 15:35:27 by welee             #+#    #+#             */
-/*   Updated: 2024/03/16 17:15:16 by welee            ###   ########.fr       */
+/*   Created: 2024/03/16 17:16:24 by welee             #+#    #+#             */
+/*   Updated: 2024/03/16 17:16:44 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.h"
-#include "check.h"
+#ifndef CHECK_DICT_FILE_H
+# define CHECK_DICT_FILE_H
 
-int	main(int argc, char **argv)
-{
-	if (!check_arg(argc, argv))
-	{
-		ft_error("Error\n");
-		return (1);
-	}
-	return (0);
-}
+int	check_dict_file(char *file_name);
+
+#endif
--- srcs/check/check_dict_file.c
+++ srcs/check/check_dict_file.c
@@ -1,24 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   check_dict_file.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/16 15:35:27 by welee             #+#    #+#             */
-/*   Updated: 2024/03/16 17:15:16 by welee            ###   ########.fr       */
+/*   Created: 2024/03/16 17:06:42 by welee             #+#    #+#             */
+/*   Updated: 2024/03/16 17:14:55 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.h"
-#include "check.h"
-
-int	main(int argc, char **argv)
+/// @brief check if the dictionary file is valid
+/// @param file_name dictionary file name
+/// @return 1 if the dictionary file is valid, 0 otherwise
+int	check_dict_file(char *file_name)
 {
-	if (!check_arg(argc, argv))
-	{
-		ft_error("Error\n");
-		return (1);
-	}
-	return (0);
+	(void)file_name;
+	return (1);
 }
