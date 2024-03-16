bb270f4f46f1d452a308a261a54657c4334e96a5
Justin Lee <triplez@justinlee.sg>
2024-03-16 15:44:17 +0800

    add ft_error to output to STDERR_FILENO

--- include/ft.h
+++ include/ft.h
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 15:18:24 by welee             #+#    #+#             */
-/*   Updated: 2024/03/16 15:21:46 by welee            ###   ########.fr       */
+/*   Updated: 2024/03/16 15:43:28 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FT_H
 
 # include "ft_atoi.h"
+# include "ft_error.h"
 # include "ft_putchar.h"
 # include "ft_putstr.h"
 # include "ft_strcmp.h"
