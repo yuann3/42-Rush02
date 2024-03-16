baf9540805ddcda26dda87b0ee64f089fb5fe2c0
Yiyuan <etherealdt@gmail.com>
2024-03-16 12:22:17 +0800

    finish ft_util.c

--- src/ft_util.c
+++ src/ft_util.c
@@ -1,4 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_util.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiyli <etherealdt@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/16 12:22:09 by yiyli             #+#    #+#             */
+/*   Updated: 2024/03/16 12:22:09 by yiyli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft.h"
-#include <stdlib.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
+		i++;
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		result = (str[i] - '0') + (result * 10);
+		i++;
+	}
+	return (sign * result);
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	dup = (char *)malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
