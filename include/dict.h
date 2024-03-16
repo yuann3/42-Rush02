65d137e5564ee7fcd2f3fafc323a3b91ce743243
mamu <mamu@c2r6s9.42singapore.sg>
2024-03-16 15:13:26 +0800

    Setup main.h

--- include/dict.h
+++ include/dict.h
@@ -1,13 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   dict_mock.h                                        :+:      :+:    :+:   */
+/*   dict.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamu <mamu@c2r6s9.42singapore.sg>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/16 15:11:05 by mamu              #+#    #+#             */
-/*   Updated: 2024/03/16 15:11:05 by mamu             ###   ########.fr       */
+/*   Created: 2024/03/16 15:10:57 by mamu              #+#    #+#             */
+/*   Updated: 2024/03/16 15:12:30 by mamu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef DICT_H
+# define DICT_H
 
+# include "dict_get.h"
+# include "dict_mock.h"
+
+typedef struct s_dict
+{
+	char	*keys;
+	char	*values;
+}	t_dict;
+
+#endif
