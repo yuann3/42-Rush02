94991cc884666ecb2fb4cb3416142370830a270b
John Xiang <qxiang@c2r5s12.42singapore.sg>
2024-03-16 18:05:00 +0800

    dict_get update

--- srcs/dict/dict_get.c
+++ srcs/dict/dict_get.c
@@ -20,7 +20,7 @@ char	*dict_get(t_dict *dict, char *key)
 	i = 0;
 	while (i < dict->size)
 	{
-		if (strcmp(key, dict->keys[i]) == 0)
+		if (ft_strcmp(key, dict->keys[i]) == 0)
 			return (dict->values[i]);
 		i++;
 	}
