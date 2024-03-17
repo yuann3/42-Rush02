a6033efa19ee7f69793004f4de1811e0b1a91b7c
John Xiang <qxiang@c2r5s12.42singapore.sg>
2024-03-17 19:14:16 +0800

    parser update

--- srcs/dict/dict_parse.c
+++ srcs/dict/dict_parse.c
@@ -21,22 +21,20 @@
 int	dict_parse(t_dict *origin, char *str)
 {
 	int			i;
+	int			err_check;
 	char		*number;
 	char		*word;
 
 	i = 0;
+	err_check = 0;
 	while (str[i])
 	{
 		number = malloc(sizeof(char) * 64);
 		word = malloc(sizeof(char) * 64);
-		if (!number || !word)
-		{
-			free(number);
-			free(word);
+		err_check = dict_getnbr(number, str + i);
+		if (!number || !word || err_check == -1)
 			return (-1);
-		}
-		i += dict_skip_non_numeric(str + i);
-		i += dict_getnbr(number, str + i);
+		i += err_check;
 		i += dict_skip_to_word(str + i);
 		i += dict_getword(word, str + i);
 		i += dict_skip_to_next_entry(str + i);
