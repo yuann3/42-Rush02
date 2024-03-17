f6fff49f657124a6a57e6e9376d7d9313d188033
John Xiang <qxiang@c2r5s12.42singapore.sg>
2024-03-17 19:27:30 +0800

    nbr break error handling update

--- srcs/nbr/nbr_break.c
+++ srcs/nbr/nbr_break.c
@@ -17,10 +17,15 @@
 
 /// @brief print a string followed by a newline
 /// @param str string
-void	ft_putstrsp(char *str)
+void	ft_putstrsp(t_dict *dict, char *str)
 {
-	ft_putstr(str);
-	ft_putchar(' ');
+	if (dict_get(dict, str) == 0)
+		exit(1);
+	else
+	{
+		ft_putstr(dict_get(dict, str));
+		ft_putchar(' ');
+	}
 }
 
 /// @brief print the ones place of a number
@@ -28,7 +33,7 @@ void	ft_putstrsp(char *str)
 /// @param str number
 void	nbr_print_ones(t_dict *dict, char *str)
 {
-	ft_putstrsp(dict_get(dict, str));
+	ft_putstrsp(dict, str);
 }
 
 /// @brief print the tens place of a number
@@ -49,10 +54,10 @@ void	nbr_print_tens(t_dict *dict, char *str)
 	if (len == 1)
 		nbr_print_ones(dict, nbr_substr(str, 0, 0));
 	else if (str[0] == '1')
-		ft_putstrsp(dict_get(dict, str));
+		ft_putstrsp(dict, str);
 	else
 	{
-		ft_putstrsp(dict_get(dict, nbr_pad_zero(str[0], 1)));
+		ft_putstrsp(dict, nbr_pad_zero(str[0], 1));
 		if (str[1] != '0')
 			nbr_print_ones(dict, nbr_substr(str, 1, 1));
 	}
@@ -65,7 +70,7 @@ void	nbr_print_hundreds(t_dict *dict, char *str)
 	len = ft_strlen(str);
 	if (len == 1 && str[0] == '0')
 	{
-		ft_putstrsp(dict_get(dict, "0"));
+		ft_putstrsp(dict, "0");
 		return ;
 	}
 	while (*str == '0')
@@ -76,13 +81,13 @@ void	nbr_print_hundreds(t_dict *dict, char *str)
 	if (len == 0)
 		return ;
 	if (len == 1)
-		ft_putstrsp(dict_get(dict, str));
+		ft_putstrsp(dict, str);
 	else if (len == 2)
 		nbr_print_tens(dict, nbr_substr(str, 0, 1));
 	else
 	{
 		nbr_print_ones(dict, nbr_substr(str, 0, 0));
-		ft_putstrsp(dict_get(dict, "100"));
+		ft_putstrsp(dict, "100");
 		nbr_print_tens(dict, nbr_substr(str, 1, 2));
 	}
 }
@@ -104,7 +109,7 @@ void	nbr_break(t_dict *dict, char *str)
 			n = 3;
 		nbr_print_hundreds(dict, nbr_substr(str, i, i + n - 1));
 		if (remain - n >= 3)
-			ft_putstrsp(dict_get(dict, nbr_pad_zero('1', remain - n)));
+			ft_putstrsp(dict, nbr_pad_zero('1', remain - n));
 		i += n;
 		remain -= n;
 	}
