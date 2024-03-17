ca131e1433fefb81f78ba6452710f12e4e56552e
mamu <mamu@c2r6s9.42singapore.sg>
2024-03-17 17:21:07 +0800

    Rename data files

--- include/check/check_dict_file.h
+++ include/check/check_dict_file.h
@@ -6,14 +6,14 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 17:16:24 by welee             #+#    #+#             */
-/*   Updated: 2024/03/16 20:43:55 by welee            ###   ########.fr       */
+/*   Updated: 2024/03/17 17:20:56 by mamu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECK_DICT_FILE_H
 # define CHECK_DICT_FILE_H
 
-# define DEFAULT_DICT "data/numbers.dict"
+# define DEFAULT_DICT "data/numbers.en.dict"
 
 int	check_dict_file(char *file_name);
 
