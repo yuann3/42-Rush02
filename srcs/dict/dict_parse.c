43020caeec2e8e7c6e995d4dfa5bb8efed2694ec
John Xiang <qxiang@c2r5s12.42singapore.sg>
2024-03-17 13:32:18 +0800

    Parser update

--- srcs/dict/dict_parse.c
+++ srcs/dict/dict_parse.c
@@ -6,43 +6,72 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 16:27:44 by qxiang            #+#    #+#             */
-/*   Updated: 2024/03/16 21:10:39 by welee            ###   ########.fr       */
+/*   Updated: 2024/03/17 11:47:20 by qxiang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "dict.h"
 #include "ft.h"
+#include <stdio.h>
+
+static int	skip_non_numeric(char *str)
+{
+    int i = 0;
+    while (str[i] && !(str[i] >= '0' && str[i] <= '9'))
+        i++;
+    return i;
+}
+
+static int	skip_to_word(char *str)
+{
+    int i = 0;
+    while (str[i] && str[i] != ':' && str[i] != '\n')
+        i++;
+    if (str[i] == ':')
+        i++;
+    while (str[i] == ' ')
+        i++;
+    return i;
+}
+
+static int	skip_to_next_entry(char *str)
+{
+    int i = 0;
+    if (str[i] == '\n')
+        i++;
+    return i;
+}
 
 static int	getnbr(char *dest, char *str)
 {
 	int		i;
-	char	c[2];
+	char	c[64];
 
 	i = 0;
-	c[1] = '\0';
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		c[0] = str[i];
-		ft_strcat(dest, c);
+		c[i] = str[i];
 		i++;
 	}
+	c[i] = '\0';
+	ft_strcpy(dest, c);
 	return (i);
 }
 
 static int	getword(char *dest, char *str)
 {
 	int		i;
-	char	c[2];
+	char	c[64];
 
 	i = 0;
-	c[1] = '\0';
 	while ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
 	{
-		c[0] = str[i];
-		ft_strcat(dest, c);
+		c[i] = str[i];
 		i++;
 	}
+	c[i] = '\0';
+	ft_strcpy(dest, c);
 	return (i);
 }
 
@@ -51,22 +80,30 @@ t_dict	dict_parse(char *str)
 	int			i;
 	char		*number;
 	char		*word;
-	t_dict		dict_entry;
+	t_dict		temp;
 
 	i = 0;
-	dict_entry = dict_new();
-	number = malloc(sizeof(char) * 128);
-	word = malloc(sizeof(char) * 128);
-	number = 0;
-	word = 0;
-	while (!(str[i] >= '0' && str[i] <= '9'))
-		i++;
-	if (str[i] >= '0' && str[i] <= '9')
+	temp = dict_new();
+	while (str[i])
+	{
+		number = malloc(sizeof(char) * 64);
+		word = malloc(sizeof(char) * 64);
+		if (!number || !word)
+		{
+			free(number);
+			free(word);
+			return (temp);
+		}
+		i += skip_non_numeric(str + i);
 		i += getnbr(number, str + i);
-	while (!(str[i] >= 65 && str[i] <= 90) || !(str[i] >= 97 && str[i] <= 122))
-		i++;
-	if (str[i] != '\n')
+		i += skip_to_word(str + i);
 		i += getword(word, str + i);
-	dict_set(&dict_entry, number, word);
-	return (dict_entry);
+		i += skip_to_next_entry(str + i);
+		printf("number: %s\n", number);
+		printf("word: %s\n", word);
+		dict_set(&temp, number, word);
+		free(number);
+		free(word);
+	}
+	return (temp);
 }
