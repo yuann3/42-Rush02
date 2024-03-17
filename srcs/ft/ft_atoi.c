068c7d4bc63b7e7b9ca2f286d98f3169de8f1ed2
John Xiang <qxiang@c2r5s12.42singapore.sg>
2024-03-17 19:21:17 +0800

    ft_atoi_check function update

--- srcs/ft/ft_atoi.c
+++ srcs/ft/ft_atoi.c
@@ -16,26 +16,17 @@
 int	ft_atoi_check(const char *str)
 {
 	int	i;
-	int	sign;
-	int	result;
 
 	i = 0;
-	sign = 1;
-	result = 0;
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	if (str[i] == '-')
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] != '\0')
 	{
-		sign = -1;
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (-1);
 		i++;
 	}
-	else if (str[i] == '+')
-		i++;
-	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
-	{
-		result *= 10;
-		result += str[i] - '0';
-		i++;
-	}
-	return (result * sign);
+	return (0);
 }
