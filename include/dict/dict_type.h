bbc882d816e0d710f024f225a87453e6873d1c5a
mamu <mamu@c2r6s9.42singapore.sg>
2024-03-17 22:18:54 +0800

    Implement

--- include/dict/dict_type.h
+++ include/dict/dict_type.h
@@ -6,7 +6,7 @@
 /*   By: mamu <mamu@c2r6s9.42singapore.sg>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 15:19:57 by mamu              #+#    #+#             */
-/*   Updated: 2024/03/16 15:48:45 by mamu             ###   ########.fr       */
+/*   Updated: 2024/03/17 21:45:33 by mamu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,5 @@ typedef struct s_dict
 	char	**values;
 	int		size;
 }	t_dict;
-
-typedef struct print_number
-{
-	char	**print;
-	int		size;
-}	printout;
 
 #endif
--- srcs/nbr/nbr_break.c
+++ srcs/nbr/nbr_break.c
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 02:38:42 by mamu              #+#    #+#             */
-/*   Updated: 2024/03/17 18:42:54 by mamu             ###   ########.fr       */
+/*   Updated: 2024/03/17 22:18:51 by mamu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,25 @@
 #include "ft.h"
 #include "nbr.h"
 
-/// @brief print a string followed by a newline
-/// @param str string
-void	ft_putstrsp(t_dict *dict, printout *print, char *str)
+#include <stdio.h>
+
+static void	nbr_store_word(t_nbr_buffer *buffer, t_dict *dict, char *str)
 {
-	if (dict_get(dict, str) == 0)
+	char	*word;
+
+	word = dict_get(dict, str);
+	printf("word = %s\n", word);
+	if (!word)
 		exit(1);
-	else
-	{
-		ft_strcat(p1.print, dict_get(dict, str));
-	}
+	nbr_buffer_add(buffer, word);
 }
 
-/// @brief print the ones place of a number
-/// @param dict dictionary
-/// @param str number
-void	nbr_print_ones(t_dict *dict, printout *print, char *str)
+static void	nbr_print_ones(t_nbr_buffer *buffer, t_dict *dict, char *str)
 {
-	ft_putstrsp(dict, p1.print, str);
+	nbr_store_word(buffer, dict, str);
 }
 
-/// @brief print the tens place of a number
-/// @param dict dictionary
-/// @param str number
-void	nbr_print_tens(t_dict *dict, printout *print, char *str)
+static void	nbr_print_tens(t_nbr_buffer *buffer, t_dict *dict, char *str)
 {
 	int	len;
 
@@ -51,64 +46,55 @@ void	nbr_print_tens(t_dict *dict, printout *print, char *str)
 	if (len == 0)
 		return ;
 	if (len == 1)
-		nbr_print_ones(dict, p1.print, nbr_substr(str, 0, 0));
+		nbr_print_ones(buffer, dict, nbr_substr(str, 0, 0));
 	else if (str[0] == '1')
-		ft_putstrsp(dict, p1.print, str);
+		nbr_store_word(buffer, dict, str);
 	else
 	{
-		ft_putstrsp(dict, p1.print, nbr_pad_zero(str[0], 1));
+		nbr_store_word(buffer, dict, nbr_pad_zero(str[0], 1));
 		if (str[1] != '0')
-			nbr_print_ones(dict, p1.print, nbr_substr(str, 1, 1));
+			nbr_print_ones(buffer, dict, nbr_substr(str, 1, 1));
 	}
 }
 
-void	nbr_print_hundreds(t_dict *dict, printout *print, char *str)
+static void	nbr_print_hundreds(t_nbr_buffer *buffer, t_dict *dict, char *str)
 {
 	int	len;
 
 	len = ft_strlen(str);
 	if (len == 1 && str[0] == '0')
 	{
-		ft_putstrsp(dict, p1.print, "0");
+		nbr_store_word(buffer, dict, "0");
 		return ;
 	}
-	while (*str == '0')
-	{
-		str++;
+	while (*(str++) == '0')
 		len--;
-	}
 	if (len == 0)
 		return ;
 	if (len == 1)
-		ft_putstrsp(dict, p1.print, str);
+		nbr_store_word(buffer, dict, str);
 	else if (len == 2)
-		nbr_print_tens(dict, p1.print, nbr_substr(str, 0, 1));
+		nbr_print_tens(buffer, dict, nbr_substr(str, 0, 1));
 	else
 	{
 		if (str[0] == '0' && str[1] == '0' && str[2] == '0')
 			return ;
-		nbr_print_ones(dict, p1.print, nbr_substr(str, 0, 0));
-		ft_putstrsp(dict, printout, "100");
-		nbr_print_tens(dict, p1.print, nbr_substr(str, 1, 2));
+		nbr_print_ones(buffer, dict, nbr_substr(str, 0, 0));
+		nbr_store_word(buffer, dict, "100");
+		nbr_print_tens(buffer, dict, nbr_substr(str, 1, 2));
 	}
 }
 
 void	nbr_break(t_dict *dict, char *str)
 {
-	int		i;
-	int		n;
-	int		len;
-	int		remain;
-	printout	p1;
+	int				i;
+	int				n;
+	int				len;
+	int				remain;
+	t_nbr_buffer	*buffer;
 
+	buffer = nbr_buffer_new();
 	i = 0;
-	p1.print = malloc(sizeof(char *) * 128);
-	while (i < 128)
-	{
-		p1.print[i] = malloc(sizeof(char) * 128);
-		i++;
-	}
-	p1.size = 0;
 	len = ft_strlen(str);
 	remain = len;
 	while (i < len)
@@ -116,12 +102,11 @@ void	nbr_break(t_dict *dict, char *str)
 		n = remain % 3;
 		if (n == 0)
 			n = 3;
-		nbr_print_hundreds(dict, p1.print, nbr_substr(str, i, i + n - 1));
+		nbr_print_hundreds(buffer, dict, nbr_substr(str, i, i + n - 1));
 		if (remain - n >= 3)
-			ft_putstrsp(dict, p1.print, nbr_pad_zero('1', remain - n));
+			nbr_store_word(buffer, dict, nbr_pad_zero('1', remain - n));
 		i += n;
 		remain -= n;
 	}
-	
-	ft_putstr(p1.print);
+	nbr_buffer_print(buffer);
 }
